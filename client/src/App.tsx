3b081e3853b595baad3c6880df1d5f2f3a5b4fe6
John Schulz <jr.john.schulz@gmail.com>
2020-03-02 20:13:32 -0500

    Added Graphs to Bracket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,17 @@ import React, { useEffect, useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import P5Wrapper from "react-p5-wrapper";
-import { PythonBracketData, PythonBracketTeam } from "./types/types";
+import {
+	PythonBracketData,
+	PythonBracketTeam,
+	PythonTeamData,
+	Assignment
+} from "./types/types";
+import { Bar } from "react-chartjs-2";
+import hexToRgba from "hex-to-rgba";
+import isEqual from "lodash/isEqual";
+import Modal, { ModalTransition } from "@atlaskit/modal-dialog";
+import Button, { ButtonGroup } from "@atlaskit/button";
 
 const App: React.FC = () => {
 	const [teamData, setTeamData] = useState<PythonBracketData>({});
@@ -10,16 +20,22 @@ const App: React.FC = () => {
 	const [selectedTeam, setSelectedTeam] = useState<
 		PythonBracketTeam | undefined
 	>(undefined);
-	const [assignedMatches, setAssignedMatches] = useState<PythonBracketTeam[][]>(
-		[]
-	);
+	const [compareData, setCompareData] = useState<any>(undefined);
+	const [assignedMatches, setAssignedMatches] = useState<Assignment[]>([]);
 
+	const getTeam = async team_name => {
+		const res = await fetch("http://localhost:5000/team/" + team_name);
+		return res.json().then(data => {
+			return data;
+		});
+	};
 	const fetchData = async () => {
 		if (!dataRecieved) {
 			const res = await fetch("http://localhost:5000/");
 			res.json().then(data => {
 				setTeamData(data);
 				console.log(teamData);
+				setDataRecieved(true);
 			});
 		}
 	};
@@ -28,44 +44,38 @@ const App: React.FC = () => {
 		fetchData();
 	});
 
-	function assignMatch(info: any) {
+	function assignMatch(info: PythonBracketTeam) {
 		let newAssignedMatches = assignedMatches;
-		for (let i = newAssignedMatches.length - 1; i >= 0; i--) {
-			let [team1, team2, winner] = newAssignedMatches[i];
-			if (
-				(info.name === team1 && info.opponent === team2) ||
-				(info.name === team2 && info.opponent === team1)
-			) {
-				newAssignedMatches.splice(i);
-			}
-		}
-		newAssignedMatches.push([info.name, info.opponent, info.name]);
+		const thisAssignment: Assignment = {
+			match: [info.name, info.opponent].sort(),
+			winner: info.name
+		};
+		const refreshedAssignments = assignedMatches.filter(
+			assignment =>
+				!isEqual(assignment.match.sort(), thisAssignment.match.sort())
+		);
+		refreshedAssignments.push({
+			match: [info.name, info.opponent],
+			winner: info.name
+		});
+		console.log(refreshedAssignments);
 		fetch("http://localhost:5000/challenge", {
 			method: "post",
-			body: JSON.stringify(newAssignedMatches)
+			body: JSON.stringify(refreshedAssignments)
 		});
-		console.log(newAssignedMatches);
-		setAssignedMatches(newAssignedMatches);
+		console.log(refreshedAssignments);
+		setAssignedMatches(refreshedAssignments);
 		setDataRecieved(false);
 	}
 
 	const sketch = p5 => {
 		let width = 0;
-        let height = 0;
-        let font
-
+		let height = 0;
 		p5.setup = () => {
 			p5.createCanvas(1500, 800);
 			width = 1500;
-            height = 800;   
-            font = p5.loadFont("Matrix II Regular.otf")
+			height = 800;
 		};
-
-		// p5.myCustomRedrawAccordingToNewPropsHandler = props => {
-		// 	if (props.bracket) {
-		// 		data = props.bracket;
-		// 	}
-		// };
 
 		p5.y_map = {
 			"64": { s: 25, i: 10 },
@@ -74,60 +84,16 @@ const App: React.FC = () => {
 			"8": { s: 200, i: 94 },
 			"4": { s: 400, i: 190 },
 			"2": { s: 800, i: 382 }
-        };
-        
+		};
 
 		p5.draw = () => {
-			let x_divisions = {
+			const x_divisions = {
 				"64": 50,
 				"32": 190,
 				"16": 330,
 				"8": 470,
 				"4": 590
 			};
-
-			if (
-				teamData !== undefined &&
-				Object.entries(teamData).length === 0 &&
-				teamData.constructor === Object
-			) {
-				console.log("No Data");
-				return;
-			}
-			setDataRecieved(true);
-			let r64 = teamData.round_of_64;
-			let r32 = teamData.round_of_32;
-			let r16 = teamData.round_of_16;
-			let r8 = teamData.round_of_8;
-			let r4 = teamData.round_of_4;
-			let r2 = teamData.round_of_2;
-			let r1 = teamData.round_of_1;
-
-			Object.keys(teamData).forEach(t => {
-				if (
-					[
-						"round_of_64",
-						"round_of_32",
-						"round_of_16",
-						"round_of_8",
-						"round_of_4",
-						"round_of_2",
-						"round_of_1",
-						"method"
-					].includes(t)
-				) {
-					return;
-				}
-			});
-
-			p5.background(200);
-			p5.textAlign(p5.CENTER);
-			p5.imageMode(p5.CENTER);
-			p5.rectMode(p5.RADIUS);
-
-			// p5.image(logo, width / 2, 100, 300, 120);
-			p5.strokeWeight(2);
-
 			// background zones
 			for (let i = 64; i >= 4; i /= 2) {
 				p5.noStroke();
@@ -136,35 +102,70 @@ const App: React.FC = () => {
 				p5.rect(0, 0, x_divisions[i.toString()], height);
 				p5.rect(width - x_divisions[i.toString()], 0, width, height);
 			}
-			makeSide(p5, x_divisions, r64);
-			makeSide(p5, x_divisions, r32);
-			makeSide(p5, x_divisions, r16);
-			makeSide(p5, x_divisions, r8);
-			makeSide(p5, x_divisions, r4);
-			drawTeam(
-				p5,
-				r2[0],
-				width / 2 - 50,
-				height / 2 - 100,
-				p5.textWidth(r2[0].name) + 50,
-				1.5
-			);
-			drawTeam(
-				p5,
-				r2[1],
-				width / 2 + 50,
-				height / 2 + 100,
-				p5.textWidth(r2[1].name) + 50,
-				1.5
-			);
-			drawTeam(
-				p5,
-				r1[0],
-				width / 2,
-				height / 2,
-				p5.textWidth(r1[0].name) + 50,
-				2.2
-			);
+			if (dataRecieved) {
+				let r64 = teamData.round_of_64;
+				let r32 = teamData.round_of_32;
+				let r16 = teamData.round_of_16;
+				let r8 = teamData.round_of_8;
+				let r4 = teamData.round_of_4;
+				let r2 = teamData.round_of_2;
+				let r1 = teamData.round_of_1;
+
+				Object.keys(teamData).forEach(t => {
+					if (
+						[
+							"round_of_64",
+							"round_of_32",
+							"round_of_16",
+							"round_of_8",
+							"round_of_4",
+							"round_of_2",
+							"round_of_1",
+							"method"
+						].includes(t)
+					) {
+						return;
+					}
+				});
+
+				p5.background(200);
+				p5.textAlign(p5.CENTER);
+				p5.imageMode(p5.CENTER);
+				p5.rectMode(p5.RADIUS);
+
+				// p5.image(logo, width / 2, 100, 300, 120);
+				p5.strokeWeight(2);
+
+				makeSide(p5, x_divisions, r64);
+				makeSide(p5, x_divisions, r32);
+				makeSide(p5, x_divisions, r16);
+				makeSide(p5, x_divisions, r8);
+				makeSide(p5, x_divisions, r4);
+				drawTeam(
+					p5,
+					r2[0],
+					width / 2 - 50,
+					height / 2 - 100,
+					p5.textWidth(r2[0].name) + 50,
+					1.5
+				);
+				drawTeam(
+					p5,
+					r2[1],
+					width / 2 + 50,
+					height / 2 + 100,
+					p5.textWidth(r2[1].name) + 50,
+					1.5
+				);
+				drawTeam(
+					p5,
+					r1[0],
+					width / 2,
+					height / 2,
+					p5.textWidth(r1[0].name) + 50,
+					2.2
+				);
+			}
 		};
 
 		const makeSide = (p5, x_divisions, arr) => {
@@ -173,7 +174,7 @@ const App: React.FC = () => {
 			p5.textSize(12);
 			for (let i = 0; i < arr.length; i++) {
 				if (p5.textWidth(arr[i].name) > maxW) {
-					maxW = p5.textWidth(arr[i].name + "    ");
+					maxW = p5.textWidth(arr[i].name);
 				}
 			}
 			for (let i = 0; i < arr.length; i++) {
@@ -230,23 +231,19 @@ const App: React.FC = () => {
 
 		const drawTeam = (p5, info, centerx, centery, w, s) => {
 			info.drawerOpen = false;
-			let name =
-				info.name +
-				"(" +
-				(info.seed + 1) +
-				") - " +
-				(info.overall_chance * 100).toFixed(0) +
-				"%, " +
-				(info.matchup_chance * 100).toFixed(0) +
-				"%";
+			let name = info.name + "(" + (info.seed + 1) + ")";
+
 			let h = 10;
 			let gap = 4;
 			p5.textSize(s * 12);
 			w *= s;
 			h *= s;
 			gap *= s;
-			p5.strokeWeight(2);
-			p5.stroke(0);
+			p5.fill(0, 30);
+			p5.strokeWeight(0);
+			p5.rect(centerx, centery + 3, w * 0.8 + 1, h, h * 0.25);
+			p5.strokeWeight(1);
+			p5.stroke(0, 100);
 			p5.fill(200, 255, 200);
 			if (info.matchup_chance < 0.53) {
 				p5.fill(255, 200, 200);
@@ -254,18 +251,73 @@ const App: React.FC = () => {
 				p5.fill(255, p5.map(info.matchup_chance, 0.53, 0.65, 200, 255), 200);
 			}
 			p5.rectMode(p5.RADIUS);
-			p5.rect(centerx, centery, w * 0.8, h, h);
+			p5.rect(centerx, centery, w * 0.8, h, h * 0.25);
 			if (
+				!selectedTeam &&
 				p5.mouseX < centerx + w * 0.8 &&
 				p5.mouseX > centerx - w * 0.8 &&
 				p5.mouseY > centery - h &&
 				p5.mouseY < centery + h
 			) {
 				p5.fill(0, 90);
-				p5.rect(centerx, centery, w * 0.8, h, h);
+				p5.rect(centerx, centery, w * 0.8, h, h * 0.25);
+				const infobox_text =
+					"Chance to get here: " +
+					(info.overall_chance * 100).toFixed(0) +
+					"%\nChance to win previous match: " +
+					(info.matchup_chance * 100).toFixed(0) +
+					"%";
+				const infobox_width = 200 * s;
+				const infobox_height = 40 * s;
+				const infobox_y = p5.mouseY - infobox_height;
+				const infobox_x = p5.mouseX;
+				const margin = 5 * s;
+				p5.textAlign(p5.LEFT);
+				p5.rectMode(p5.CORNER);
+				p5.rect(infobox_x, infobox_y, infobox_width, infobox_height, h * 0.25);
+				p5.fill(255, 200);
+				p5.strokeWeight(0);
+				p5.text(infobox_text, infobox_x + margin, infobox_y + 3 * margin);
+				p5.textAlign(p5.CENTER);
+				p5.rectMode(p5.CENTER);
 				if (p5.mouseIsPressed) {
-					assignMatch(info);
+					// assignMatch(info);
 					setSelectedTeam(info);
+					getTeam(info.name).then((home_data: PythonTeamData) => {
+						getTeam(info.opponent).then((away_data: PythonTeamData) => {
+							console.log(home_data);
+							console.log(away_data);
+							setCompareData({
+								labels: ["SOS", "Off. Eff.", "Def. Eff.", "FG %"],
+								datasets: [
+									{
+										label: info.name,
+										backgroundColor: hexToRgba(home_data.main_color, 0.6),
+										borderColor: hexToRgba(home_data.main_color, 1),
+										borderWidth: 1,
+										data: [
+											home_data.sos,
+											home_data.offensive,
+											home_data.defensive,
+											home_data.fg_pct * 100
+										]
+									},
+									{
+										label: info.opponent,
+										backgroundColor: hexToRgba(away_data.main_color, 0.6),
+										borderColor: hexToRgba(away_data.main_color, 1),
+										borderWidth: 1,
+										data: [
+											away_data.sos,
+											away_data.offensive,
+											away_data.defensive,
+											away_data.fg_pct * 100
+										]
+									}
+								]
+							});
+						});
+					});
 				}
 			}
 			p5.strokeWeight(0);
@@ -276,9 +328,29 @@ const App: React.FC = () => {
 
 	return (
 		<div className="App">
-			{selectedTeam && <h1>{selectedTeam.name}</h1>}
-			{selectedTeam && <h1>{selectedTeam.opponent}</h1>}
+			{selectedTeam && compareData && (
+				<ModalTransition>
+					<Modal
+						actions={[
+							{ text: `Set ${selectedTeam.name} to winner` },
+							{ text: `Set ${selectedTeam.opponent} to winner` }
+						]}
+						heading={`${selectedTeam.name} vs ${selectedTeam.opponent}`}
+						onClose={() => setSelectedTeam(undefined)}
+					>
+						<Bar data={compareData} width={800} height={400}></Bar>
+					</Modal>
+				</ModalTransition>
+			)}
 			<P5Wrapper sketch={sketch} bracket={teamData} />
+			{assignedMatches.map(assignment => {
+				return (
+					<div>
+						<h1>{assignment.match}</h1>
+						<h2>{assignment.winner}</h2>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
--- client/src/types/types.tsx
+++ client/src/types/types.tsx
@@ -17,9 +17,75 @@ export interface PythonBracketData {
 export interface PythonBracketTeam {
 	matchup_chance: number;
 	overall_chance: number;
-    seed: number;
-    opponent: string;
+	seed: number;
+	opponent: string;
 	name: string;
-    drawerOpen: boolean;
-    teamId: number
+	winner: string;
+}
+
+export interface Assignment {
+	match: string[];
+	winner: string;
+}
+
+export interface Player {
+	name: string;
+	started: number;
+	minutes: number;
+	fga: number;
+	fg: number;
+	fg2a: number;
+	fg2: number;
+	fg3a: number;
+	fg3: number;
+	fta: number;
+	ft: number;
+	orb: number;
+	drb: number;
+	ast: number;
+	stl: number;
+	blk: number;
+	tov: number;
+	pf: number;
+	pts: number;
+}
+export interface PythonTeamData {
+	g: number;
+	mp: number;
+	fg: number;
+	fga: number;
+	fg_pct: number;
+	fg2: number;
+	fg2a: number;
+	fg2_pct: number;
+	fg3: number;
+	fg3a: number;
+	fg3_pct: number;
+	ft: number;
+	fta: number;
+	ft_pct: number;
+	orb: number;
+	drb: number;
+	trb: number;
+	ast: number;
+	stl: number;
+	blk: number;
+	tov: number;
+	pf: number;
+	pts: number;
+	pts_per_g: number;
+	players: Player[];
+	weights: number[];
+	heights: number[];
+	jersey_nums: number[];
+	hometowns: string[];
+	player_pts: number[];
+	offensive: number;
+	defensive: number;
+	sos: number;
+	points_scored: number;
+	points_against: number;
+	logo_url: string;
+	main_color: string;
+	wins: Object;
 }
